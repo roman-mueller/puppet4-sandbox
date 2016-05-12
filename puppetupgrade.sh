6c1afaf2107aec8dc4ff5096fe2ce45f175a703e
Roman Mueller <roman.mueller@gmail.com>
2016-05-12 14:47:27 +0200

    Fix typos

--- puppetupgrade.sh
+++ puppetupgrade.sh
@@ -7,14 +7,14 @@ if [ ! -f /etc/provisioned ] ; then
   # install Puppet 4.x release repo
   /bin/yum -y install https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
   if [ $? -ne 0 ] ; then
-    echo "Something went wrong intalling the repository RPM"
+    echo "Something went wrong installing the repository RPM"
     exit 1
   fi
 
   # install / update puppet-agent
   /bin/yum -y install puppet-agent
   if [ $? -ne 0 ] ; then
-    echo "Something went wrong intalling puppet-agent"
+    echo "Something went wrong installing puppet-agent"
     exit 1
   fi
 
